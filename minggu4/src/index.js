e918b45a3401c716de195a58a44a55d87140dc85
Andhika Utama <andhikautamaa04@gmail.com>
2021-03-04 16:06:06 +0700

    Minggu keempat

--- minggu4/src/index.js
+++ minggu4/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import BlogPost from "./container/BlogPost/BlogPost";
 
