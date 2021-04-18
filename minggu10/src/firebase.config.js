82b17123b8cba337bd9c0611e63359223094413a
Andhika Utama <andhikautamaa04@gmail.com>
2021-04-18 09:38:26 +0700

    Done

---
+++ minggu10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyB685sn66smGh1-6tCQ-f8F1WggrQ6F_QQ",
+    authDomain: "minggu10-8eac6.firebaseapp.com",
+    projectId: "minggu10-8eac6",
+    storageBucket: "minggu10-8eac6.appspot.com",
+    messagingSenderId: "3758110849",
+    appId: "1:3758110849:web:ef4a7d234510f4bae17580",
+    measurementId: "G-D9X7HVLE9B"
+};
