5b56903b0ff81556eeb76805cc2881a56106f992
Andhika Utama <andhikautamaa04@gmail.com>
2021-05-02 09:56:10 +0700

    Pertemuan 12

---
+++ minggu12/crudfirebase/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB685sn66smGh1-6tCQ-f8F1WggrQ6F_QQ",
+    authDomain: "minggu10-8eac6.firebaseapp.com",
+    databaseURL: "https://minggu10-8eac6-default-rtdb.firebaseio.com",
+    projectId: "minggu10-8eac6",
+    storageBucket: "minggu10-8eac6.appspot.com",
+    messagingSenderId: "3758110849",
+    appId: "1:3758110849:web:75a705a967b9f099e17580",
+    measurementId: "G-KKBX3HNBWJ"
+};
+
+export default firebaseConfig;
